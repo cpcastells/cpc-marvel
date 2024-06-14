a2ffce0f5bc708cc031d14b0c255583dc876acde
Carles <cpcastells@gmail.com>
2024-06-14 22:28:13 +0200

    Add GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { ThemeProvider } from "styled-components";
 import "@fontsource/roboto-condensed";
 import App from "./components/App/App.tsx";
 import { theme } from "./styles/theme/theme.ts";
+import { GlobalStyle } from "./styles/GlobalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  ::before,
+  ::after {
+    box-sizing: border-box;
+    
+  }
+
+  html {
+    font-family: ${(props) => props.theme.fonts.primary};
+    -webkit-appearance: none;
+    appearance: none;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.background}; 
+    color: ${(props) => props.theme.colors.background}; 
+    margin: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  p {
+    margin: 0;
+    font: inherit;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+
+  input {
+    border: none;
+    background: none;
+    font: inherit;
+  }
+`;
