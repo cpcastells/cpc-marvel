c6d4fb2aaf3cc00a6e7853944c150d4e5e186af1
Carles <cpcastells@gmail.com>
2024-06-15 20:05:43 +0200

    Update appRouter test with renderWithProviders

--- src/routers/appRouter/appRouter.test.tsx
+++ src/routers/appRouter/appRouter.test.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import appRouter from "./appRouter";
+import { renderWithProviders } from "../../utils/testUtils";
 
 describe("Given an appRouter", () => {
   describe("When a user enters the app", () => {
     test("Then it should show the Marvel logo ", () => {
       const altText = "Marvel logo";
 
-      render(<RouterProvider router={appRouter} />);
+      renderWithProviders(<RouterProvider router={appRouter} />);
       const headingElement = screen.getByAltText(altText);
 
       expect(headingElement).toBeInTheDocument();
