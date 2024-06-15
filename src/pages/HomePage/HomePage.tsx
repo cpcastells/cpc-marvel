c6a2c83e7222b560a57934291153d809c5df474b
Carles <cpcastells@gmail.com>
2024-06-15 02:20:03 +0200

    Add SearchBar in HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
 import HomePageStyled from "./HomePageStyled";
 
 const HomePage = (): React.ReactElement => {
-  return <HomePageStyled />;
+  return (
+    <HomePageStyled>
+      <SearchBar />
+    </HomePageStyled>
+  );
 };
 
 export default HomePage;
