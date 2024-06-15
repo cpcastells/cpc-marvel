bad98cc9ccaaaab59719b7a3b9c8c2dc290ffe9a
Carles <cpcastells@gmail.com>
2024-06-16 00:48:16 +0200

    Update HomePage to display favorites when in favorites view

--- src/pages/HomePage/HomePageStyled.tsx
+++ src/pages/HomePage/HomePageStyled.tsx
@@ -1,12 +1,36 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import toRem from "../../styles/utils";
 
-const HomePageStyled = styled.main`
+interface HomePageStyledProps {
+  $isFavoritesView: boolean;
+}
+
+const HomePageStyled = styled.main<HomePageStyledProps>`
   display: flex;
   flex-direction: column;
   gap: ${toRem(24)};
   padding: ${toRem(24)} ${toRem(16)};
   text-transform: uppercase;
+  transition: padding 0.5s ease;
+
+  .favorites-title {
+    font-size: ${toRem(24)};
+    color: #000;
+    font-weight: 700;
+    opacity: 0;
+    height: 0;
+    overflow: hidden;
+    transition:
+      opacity 0.5s ease-in-out,
+      height 0.3s ease-in-out;
+
+    ${({ $isFavoritesView }) =>
+      $isFavoritesView &&
+      css`
+        opacity: 1;
+        height: ${toRem(32)};
+      `}
+  }
 
   @media (min-width: 834px) {
     padding: ${toRem(48)};
