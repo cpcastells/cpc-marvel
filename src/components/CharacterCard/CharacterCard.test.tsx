4137cd47b8f70b7f8a82b95868e498f81df460bc
Carles <cpcastells@gmail.com>
2024-06-15 22:02:35 +0200

    Add CharacterCard tests - render add and remove buttons

--- src/components/CharacterCard/CharacterCard.test.tsx
+++ src/components/CharacterCard/CharacterCard.test.tsx
@@ -4,9 +4,9 @@ import CharacterCard from "./CharacterCard";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 
 describe("Given a CharacterCard component", () => {
-  describe("When it receives a character with the name 'Spiderman'", () => {
-    const characterMock = createMockCharacter({ name: "Spiderman" });
+  const characterMock = createMockCharacter({ name: "Spiderman" });
 
+  describe("When it receives a character with the name 'Spiderman'", () => {
     test("Then it should display the character's name", () => {
       renderWithProviders(
         wrapWithRouter(<CharacterCard character={characterMock} />),
@@ -34,6 +34,37 @@ describe("Given a CharacterCard component", () => {
 
       renderWithProviders(
         wrapWithRouter(<CharacterCard character={characterMock} />),
+      );
+      const favoriteButton = screen.getByRole("button", {
+        name: buttonDescription,
+      });
+
+      expect(favoriteButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives a character with the name 'Spiderman' and the character is not in favorites", () => {
+    test("Then it should display the 'Add to favorites' button", () => {
+      const buttonDescription = "Add to favorites";
+
+      renderWithProviders(
+        wrapWithRouter(<CharacterCard character={characterMock} />),
+      );
+      const favoriteButton = screen.getByRole("button", {
+        name: buttonDescription,
+      });
+
+      expect(favoriteButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives a character with the name 'Spiderman' and the character is in favorites", () => {
+    test("Then it should display the 'Remove from favorites' button", () => {
+      const buttonDescription = "Remove from favorites";
+
+      renderWithProviders(
+        wrapWithRouter(<CharacterCard character={characterMock} />),
+        { preloadedFavorites: [characterMock] },
       );
       const favoriteButton = screen.getByRole("button", {
         name: buttonDescription,
