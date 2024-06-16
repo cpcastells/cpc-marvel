ba1ebea0c5a31242aab06c2d9f66272d10e26e1e
Carles <cpcastells@gmail.com>
2024-06-16 21:55:04 +0200

    Add LoaderBar to CharacterPage

--- src/pages/CharacterPage/CharacterPage.tsx
+++ src/pages/CharacterPage/CharacterPage.tsx
@@ -6,10 +6,11 @@ import useCharacters from "../../hooks/useCharacters/useCharacters";
 import { Character } from "../../types";
 import SolidFavoriteIcon from "../../components/icons/SolidFavoriteIcon";
 import UnselectedFavoriteIcon from "../../components/icons/UnselectedFavoriteIcon";
+import LoaderBar from "../../components/LoaderBar/LoaderBar";
 
 const CharacterPage = () => {
   const { characterId } = useParams();
-  const { getCharacterById } = useCharacters();
+  const { getCharacterById, isCharacterByIdLoading } = useCharacters();
   const [character, setCharacter] = useState<Character | null>(null);
   const { favorites, addFavorite, removeFavorite } = useFavorites();
   const isFavorite = favorites.some((fav) => fav.id === Number(characterId!));
@@ -27,58 +28,57 @@ const CharacterPage = () => {
     })();
   }, [characterId, getCharacterById]);
 
-  if (!character) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <CharacterPageStyled>
-      <article className="character-detail">
-        <header className="character-detail__header">
-          <div className="character-detail__header-content">
-            <img
-              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-              alt="Heeman"
-              className="character-detail__image"
-            />
-            <div className="character-detail__info">
-              <div className="character-detail__header-row">
-                <h1 className="character-detail__name">{character.name}</h1>
-                <button
-                  className="character-detail__favorite"
-                  aria-label={
-                    isFavorite ? "Remove from favorites" : "Add to favorites"
-                  }
-                  onClick={() => {
-                    isFavorite
-                      ? removeFavorite(character)
-                      : addFavorite(character);
-                  }}
-                >
-                  <SolidFavoriteIcon
-                    height={24}
-                    width={22}
-                    className={`heart-icon ${isFavorite ? "heart-icon--active" : ""}`}
-                  />
-                  <UnselectedFavoriteIcon
-                    height={24}
-                    width={22}
-                    className={`heart-icon ${!isFavorite ? "heart-icon--active" : ""}`}
-                  />
-                </button>
+      <LoaderBar isLoading={isCharacterByIdLoading} />
+      {!isCharacterByIdLoading && character && (
+        <article className="character-detail">
+          <header className="character-detail__header">
+            <div className="character-detail__header-content">
+              <img
+                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                alt={`Image of ${character.name}`}
+                className="character-detail__image"
+              />
+              <div className="character-detail__info">
+                <div className="character-detail__header-row">
+                  <h1 className="character-detail__name">{character.name}</h1>
+                  <button
+                    className="character-detail__favorite"
+                    aria-label={
+                      isFavorite ? "Remove from favorites" : "Add to favorites"
+                    }
+                    onClick={() => {
+                      isFavorite
+                        ? removeFavorite(character)
+                        : addFavorite(character);
+                    }}
+                  >
+                    <SolidFavoriteIcon
+                      height={24}
+                      width={22}
+                      className={`heart-icon ${isFavorite ? "heart-icon--active" : ""}`}
+                    />
+                    <UnselectedFavoriteIcon
+                      height={24}
+                      width={22}
+                      className={`heart-icon ${!isFavorite ? "heart-icon--active" : ""}`}
+                    />
+                  </button>
+                </div>
+                <p className="character-detail__description">
+                  {character.description || "No description available."}
+                </p>
+                <div className="character-detail__corner" />
               </div>
-              <p className="character-detail__description">
-                {character.description || "No description available."}
-              </p>
-              <div className="character-detail__corner" />
             </div>
-          </div>
-        </header>
-        <section className="character-detail__comics">
-          <h2>Comics</h2>
-          SLIDER
-        </section>
-      </article>
+          </header>
+          <section className="character-detail__comics">
+            <h2>Comics</h2>
+            SLIDER
+          </section>
+        </article>
+      )}
     </CharacterPageStyled>
   );
 };
