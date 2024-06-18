a3a0b9aa505eeafd9681962bb4a28c376b916040
Carles <cpcastells@gmail.com>
2024-06-19 01:12:25 +0200

    Remove draggable behaviour of comic card images

--- src/components/ComicCard/ComicCard.tsx
+++ src/components/ComicCard/ComicCard.tsx
@@ -5,16 +5,22 @@ interface ComicCardProps {
   comic: Comic;
 }
 
-const ComicCard = ({ comic }: ComicCardProps) => (
-  <ComicCardStyled>
-    <div className="comic__image-container">
-      <img src={comic.thumbnail} alt={`Image of ${comic.title}`} />
-    </div>
-    <div className="comic__info-container">
-      <h3 className="comic__title">{comic.title}</h3>
-      <div className="comic__date">{comic.onSaleDate}</div>
-    </div>
-  </ComicCardStyled>
-);
+const ComicCard = ({ comic }: ComicCardProps) => {
+  const handleDragStart = (e: React.DragEvent<HTMLLIElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <ComicCardStyled onDragStart={handleDragStart}>
+      <div className="comic__image-container">
+        <img src={comic.thumbnail} alt={`Image of ${comic.title}`} />
+      </div>
+      <div className="comic__info-container">
+        <h3 className="comic__title">{comic.title}</h3>
+        <div className="comic__date">{comic.onSaleDate}</div>
+      </div>
+    </ComicCardStyled>
+  );
+};
 
 export default ComicCard;
