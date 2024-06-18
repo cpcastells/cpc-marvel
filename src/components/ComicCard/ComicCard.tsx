184f4841cbf87d5ecc96465202464103ffb64fbe
Carles <cpcastells@gmail.com>
2024-06-18 02:35:09 +0200

    Create ComicCard component

---
+++ src/components/ComicCard/ComicCard.tsx
@@ -0,0 +1,20 @@
+import { Comic } from "../../types";
+import ComicCardStyled from "./ComicCardStyled";
+
+interface ComicCardProps {
+  comic: Comic;
+}
+
+const ComicCard = ({ comic }: ComicCardProps) => (
+  <ComicCardStyled>
+    <div className="comic__image-container">
+      <img src={comic.thumbnail} alt={`Image of ${comic.title}`} />
+    </div>
+    <div className="comic__info-container">
+      <h3 className="comic__title">{comic.title}</h3>
+      <div className="comic__date">{comic.onSaleDate}</div>
+    </div>
+  </ComicCardStyled>
+);
+
+export default ComicCard;
